44e727df8dab2316b72948e2c32da11fd694d9a8
neverzhai <shuanger.zhai1990@gmail.com>
2019-03-28 08:02:55 +0800

    change url and controller.

---
+++ CoreDocker/Controllers/NameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        // GET api/name
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "shuanger" };
+        }
+    }
+}
